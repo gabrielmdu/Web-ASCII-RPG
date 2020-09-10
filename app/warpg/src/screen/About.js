e43c14ff1cd58922de8a59a283d9e7e2c8a7d951
Gabriel Schulte <gabrielmdu@gmail.com>
2020-09-10 00:02:07 -0300

    Created the about screen

---
+++ app/warpg/src/screen/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../scss/about.scss';
+
+const About = () => {
+  return (
+    <div className="about">
+      <pre>{String.raw`
+  ______  __                       __      
+ /\  _  \/\ \                     /\ \__   
+ \ \ \_\ \ \ \____    ___   __  __\ \  _\  
+  \ \  __ \ \  __ \  / __ \/\ \/\ \\ \ \/  
+   \ \ \/\ \ \ \_\ \/\ \_\ \ \ \_\ \\ \ \_ 
+    \ \_\ \_\ \____/\ \____/\ \____/ \ \__\
+     \/_/\/_/\/___/  \/___/  \/___/   \/__/ 
+`}</pre>
+      <div className="made-wrapper">
+        <div>made by</div>
+        <a
+          href="https://gabrielmdu.github.io"
+          target="blank"
+        >
+          Gabriel Schulte
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default About;

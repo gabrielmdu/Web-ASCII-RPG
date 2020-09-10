a5135c7a20e5ea725d8ec8f7b7a1f7198f3e25f2
Gabriel Schulte <gabrielmdu@gmail.com>
2020-09-10 00:03:31 -0300

    Modal now imports its own style sheet

--- app/warpg/src/screen/Modal.js
+++ app/warpg/src/screen/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../scss/modal.scss';
 
 const Modal = ({ children, handleModalHide, modalClass }) => {
   const className = modalClass ? modalClass : 'modal-common';

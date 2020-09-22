e64e4654dd324c4ef397c78b43a1976a5c98db34
Gabriel Schulte <gabrielmdu@gmail.com>
2020-09-21 21:07:28 -0300

    Game list endpoint now sends the game id

--- api/www/app/Http/Resources/GameBasicInfo.php
+++ api/www/app/Http/Resources/GameBasicInfo.php
@@ -15,6 +15,7 @@ class GameBasicInfo extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->adventure['name'],
             'description' => $this->adventure['description']
         ];

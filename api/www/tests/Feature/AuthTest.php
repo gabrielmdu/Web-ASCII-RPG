50826de66a771cfd81d86391422737034da612cf
Gabriel Schulte <gabrielmdu@gmail.com>
2020-09-30 00:03:07 -0300

    Added more auth tests

--- api/www/tests/Feature/AuthTest.php
+++ api/www/tests/Feature/AuthTest.php
@@ -2,25 +2,92 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 
+/**
+ * Class that tests methods related to user and authentication
+ */
 class AuthTest extends TestCase
 {
+    const USER_TEST_EMAIL = 'test@warpg.com';
+    const USER_TEST_PASS = '123';
+
     /**
-     * Tests if the user can login with the sample admin user credentials.
+     * Tests if a new user can be created and inserted into the database.
      *
-     * @return void
+     * @return User The created user
+     */
+    public function testCanCreateUser()
+    {
+        $user = factory(User::class)->make([
+            'email' => self::USER_TEST_EMAIL,
+            'password' => password_hash(self::USER_TEST_PASS, PASSWORD_DEFAULT),
+        ]);
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertTrue($user->save());
+
+        return $user;
+    }
+
+    /**
+     * Tests if the user can login with the sample test user credentials
+     *
+     * @depends testCanCreateUser
+     * @return string Newly created user token
      */
     public function testCanLogin()
     {
-        return $this->postJSON(
+        $token = $this->postJSON(
             '/v1/login',
             [
-                'email' => 'admin@admin.com',
-                'password' => '123',
+                'email' => self::USER_TEST_EMAIL,
+                'password' => self::USER_TEST_PASS,
             ])
             ->assertOk()
             ->assertJsonStructure(['access_token'])
             ->decodeResponseJson('access_token');
+
+        $this->assertInstanceOf(User::class, auth()->user());
+
+        return $token;
+    }
+
+    /**
+     * Tests if login can be refreshed/renewed
+     *
+     * @depends testCanLogin
+     * @param string $originalToken
+     * @return string
+     */
+    public function testCanRefreshLogin(string $originalToken)
+    {
+        $newToken = $this->postJson('/v1/refresh', [], [
+            'Authorization' => 'Bearer ' . $originalToken
+        ])
+            ->assertOk()
+            ->assertJsonStructure(['access_token'])
+            ->decodeResponseJson('access_token');
+        
+        $this->assertNotEquals($originalToken, $newToken);
+
+        return $newToken;
+    }
+    
+    /**
+     * Tests if the user can be removed
+     *
+     * @depends testCanRefreshLogin
+     * @param string $token
+     * @return void
+     */
+    public function testCanRemoveUser(string $token)
+    {
+        auth()->setToken($token);
+        $user = auth()->authenticate();
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertTrue($user->delete());
     }
 }

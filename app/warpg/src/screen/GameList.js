13fc7e5f12747ecf634ba6eb6edf004a3896f21f
Gabriel Schulte <gabrielmdu@gmail.com>
2020-10-16 17:05:41 -0300

    Added an error dialog for new game issues

--- app/warpg/src/screen/GameList.js
+++ app/warpg/src/screen/GameList.js
@@ -3,10 +3,13 @@ import { useHistory } from 'react-router-dom';
 import { fetchAuthPost, fetchGet } from '../utils';
 
 import BackToMenu from './BackToMenu';
+import ButtonsModal from './modal/ButtonsModal';
 
 import './GameList.scss';
 
 const GameList = ({ user }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalText, setModalText] = useState('');
   const [gameList, setGameList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [selectedGameId, setSelectedGameId] = useState(null);
@@ -31,48 +34,62 @@ const GameList = ({ user }) => {
 
     if (request.status === 200) {
       history.push('/play');
+    } else {
+      const response = await request.json();
+      setModalText(response.message);
+      setShowModal(true);
     }
   };
 
   return (
-    <div className="game-list">
-      <div className="title">Game List</div>
-      {isLoading
-        ? <div>Loading game list...</div>
-        : <div>
-          <div className="table-wrapper">
-            <table>
-              <thead>
-                <tr>
-                  <th>NAME</th>
-                  <th>DESCRIPTION</th>
-                </tr>
-              </thead>
-              <tbody>
-                {gameList.map(game =>
-                  <tr
-                    className={game.id === selectedGameId ? 'selected' : ''}
-                    key={game.id}
-                    onClick={() => setSelectedGameId(game.id)}
-                  >
-                    <td>{game.name}</td>
-                    <td>{game.description}</td>
-                  </tr>)}
-              </tbody>
-            </table>
-          </div>
-          <div className="game-list-buttons">
-            <button disabled={!user}>Continue</button>
-            <button
-              disabled={!selectedGameId}
-              onClick={handleNewGame}
-            >
-              New Game
+    <>
+      {showModal &&
+        <ButtonsModal
+          type="error"
+          title="Error"
+          text={modalText}
+          buttons={[{ label: "Close", handleClick: () => setShowModal(false) }]}
+        />}
+
+      <div className="game-list">
+        <div className="title">Game List</div>
+        {isLoading
+          ? <div>Loading game list...</div>
+          : <div>
+            <div className="table-wrapper">
+              <table>
+                <thead>
+                  <tr>
+                    <th>NAME</th>
+                    <th>DESCRIPTION</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {gameList.map(game =>
+                    <tr
+                      className={game.id === selectedGameId ? 'selected' : ''}
+                      key={game.id}
+                      onClick={() => setSelectedGameId(game.id)}
+                    >
+                      <td>{game.name}</td>
+                      <td>{game.description}</td>
+                    </tr>)}
+                </tbody>
+              </table>
+            </div>
+            <div className="game-list-buttons">
+              <button disabled={!user}>Continue</button>
+              <button
+                disabled={!selectedGameId}
+                onClick={handleNewGame}
+              >
+                New Game
             </button>
-          </div>
-        </div>}
-      <BackToMenu />
-    </div>
+            </div>
+          </div>}
+        <BackToMenu />
+      </div>
+    </>
   );
 };
 

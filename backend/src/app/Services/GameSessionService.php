22f50e238883ee59def23af0b6b42cf94d09c1cf
Gabriel Schulte <gabrielmdu@gmail.com>
2026-02-20 16:39:01 +0200

    Added game session created event

--- backend/src/app/Services/GameSessionService.php
+++ backend/src/app/Services/GameSessionService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Events\GameSessionCreated;
 use App\Models\Game;
 use App\Models\GameSession;
 use App\Models\User;
@@ -12,11 +13,15 @@ class GameSessionService
     {
         $game = Game::with('startScene')->findOrFail($gameId);
 
-        return $user->gameSessions()
+        $session = $user->gameSessions()
             ->create([
                 'player_id' => $user->id,
                 'game_id' => $game->id,
                 'current_scene_id' => $game->startScene->id,
             ]);
+
+        event(new GameSessionCreated($session));
+
+        return $session;
     }
 }
